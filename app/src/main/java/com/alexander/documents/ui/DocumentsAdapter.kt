6277bd4e7d79a36da2ca045cae5d0addef12e38e
Grekerochek <klik34@list.ru>
2020-02-16 19:58:25 +0300

    Initial commit

---
+++ app/src/main/java/com/alexander/documents/ui/DocumentsAdapter.kt
@@ -0,0 +1,193 @@
+package com.alexander.documents.ui
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.GradientDrawable
+import android.view.*
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.document_item.view.*
+import java.util.*
+import java.text.SimpleDateFormat
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.content.ContextCompat
+import android.util.DisplayMetrics
+import android.content.Context
+import kotlin.math.roundToInt
+import androidx.core.graphics.drawable.DrawableCompat
+import com.alexander.documents.R
+import com.alexander.documents.entity.Document
+
+/**
+ * author alex
+ */
+class DocumentsAdapter(
+    val documentClickListener: (Document) -> Unit,
+    val popUpClickListener: (Int, Int, Int) -> Unit
+) : ListAdapter<Document, RecyclerView.ViewHolder>(DiffCallback()) {
+
+    var documents: MutableList<Document> = mutableListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.document_item, parent, false)
+        return DocumentsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = documents.size
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val document = documents[position]
+        with(holder.itemView) {
+            setOnClickListener { documentClickListener(document) }
+
+            titleView.text = document.title
+            initPopUpMenu(threePointsView, holder.adapterPosition)
+
+            val date = Date(document.date * MS_TO_S)
+            val dateFormat = SimpleDateFormat(PATTERN, Locale.getDefault())
+
+            dateView.text =
+                document.ext + " \u00B7 " + dateFormat.format(date) + " \u00B7 " + document.size
+            typeView.text = document.type
+            typeView.visibility = if (document.type.isEmpty()) View.GONE else View.VISIBLE
+            setImage(document, photoView)
+        }
+    }
+
+    fun deleteDocument(position: Int) {
+        documents.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    fun renameDocument(documentTitle: String, position: Int) {
+        documents[position].title = documentTitle
+        notifyItemChanged(position)
+    }
+
+    private fun initPopUpMenu(view: View, position: Int) {
+        view.setOnClickListener {
+            val popup = PopupMenu(view.context, it, Gravity.CENTER, 0,
+                R.style.PopupMenu
+            )
+            popup.inflate(R.menu.pop_up_menu)
+            popup.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem): Boolean {
+                    when (item.itemId) {
+                        R.id.rename -> {
+                            popUpClickListener(
+                                documents[position].id,
+                                MainActivity.RENAME_ACTION,
+                                position
+                            )
+                            return true
+                        }
+                        R.id.delete -> {
+                            popUpClickListener(
+                                documents[position].id,
+                                MainActivity.DELETE_ACTION,
+                                position
+                            )
+                            return true
+                        }
+                    }
+                    return false
+                }
+            })
+            popup.show()
+        }
+    }
+
+    private fun setImage(document: Document, photoView: ImageView) {
+        if (document.photo != null) {
+            photoView.setImageDrawable(null)
+            photoView.background = null
+            photoView.setPadding(0, 0, 0, 0)
+            Glide.with(photoView.context)
+                .load(document.photo.src)
+                .apply(
+                    RequestOptions().transforms(
+                        CenterCrop(),
+                        RoundedCorners(6)
+                    )
+                )
+                .into(photoView)
+        } else {
+            val imageDrawableAndColor = getImageDrawableAndColor(document.ext)
+            photoView.setImageResource(imageDrawableAndColor.first)
+            photoView.setBackgroundResource(R.drawable.round_corners_drawable)
+            (photoView.background as GradientDrawable).apply {
+                setColor(ContextCompat.getColor(photoView.context, imageDrawableAndColor.second))
+                alpha = (100 - 16) * 255 / 100
+            }
+            val drawable = ContextCompat.getDrawable(photoView.context, imageDrawableAndColor.first)
+            DrawableCompat.setTint(drawable!!, imageDrawableAndColor.second)
+            val dp = dpToPx(photoView.context, 25)
+            photoView.setPadding(dp, dp, dp, dp)
+        }
+    }
+
+    private fun getImageDrawableAndColor(ext: String): Pair<Int, Int> {
+        return when (ext) {
+            "ZIP" -> Pair(
+                R.drawable.ic_zip,
+                R.color.colorZip
+            )
+            "MP4" -> Pair(
+                R.drawable.ic_video,
+                R.color.colorVideo
+            )
+            "MP3" -> Pair(
+                R.drawable.ic_music,
+                R.color.colorMusic
+            )
+            "PDF", "EPUB" -> Pair(
+                R.drawable.ic_book,
+                R.color.colorBook
+            )
+            "TXT", "DOCX" -> Pair(
+                R.drawable.ic_text,
+                R.color.colorText
+            )
+            else -> Pair(
+                R.drawable.ic_other,
+                R.color.colorOther
+            )
+        }
+    }
+
+    fun dpToPx(context: Context, dp: Int): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Document>() {
+        override fun areItemsTheSame(oldItem: Document, newItem: Document): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Document, newItem: Document): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class DocumentsViewHolder(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView),
+        LayoutContainer
+
+    private companion object {
+        const val PATTERN: String = "dd MMMM yyyy"
+        const val MS_TO_S: Int = 1000
+    }
+}
---
+++ app/src/main/java/com/alexander/documents/ui/MainActivity.kt
@@ -0,0 +1,177 @@
+package com.alexander.documents.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+import kotlinx.android.synthetic.main.activity_main.*
+import android.net.Uri
+import android.text.InputType
+import android.widget.EditText
+import android.widget.FrameLayout
+import com.alexander.documents.R
+import com.alexander.documents.api.DocumentsDelete
+import com.alexander.documents.api.DocumentsEdit
+import com.alexander.documents.api.DocumentsRequest
+import com.alexander.documents.entity.Document
+
+/**
+ * author alex
+ */
+class MainActivity : AppCompatActivity() {
+
+    private val documentsAdapter by lazy(LazyThreadSafetyMode.NONE) {
+        DocumentsAdapter(::openDocumentDetails, ::onPopUpMenuClick)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.elevation = 0f
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            containerView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        if (!VK.isLoggedIn()) {
+            VK.login(this, arrayListOf(VKScope.DOCS))
+        } else {
+            recyclerViewDocuments.adapter = documentsAdapter
+            containerView.setOnRefreshListener { requestDocs() }
+            requestDocs()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object : VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                requestDocs()
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                showAuthError()
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun requestDocs() {
+        containerView.isRefreshing = true
+        VK.execute(DocumentsRequest(), object : VKApiCallback<List<Document>> {
+            override fun success(result: List<Document>) {
+                if (!isFinishing) {
+                    containerView.isRefreshing = false
+                    documentsAdapter.documents = result.toMutableList()
+                }
+            }
+
+            override fun fail(error: Exception) {
+                showError()
+            }
+        })
+    }
+
+    private fun showError() {
+        containerView.isRefreshing = false
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.error))
+            .setMessage(getString(R.string.error_message))
+            .setPositiveButton(R.string.ok) { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    private fun showAuthError() {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.error))
+            .setMessage(getString(R.string.error_auth_message))
+            .setPositiveButton(R.string.ok) { _, _ -> VK.login(this, arrayListOf(VKScope.DOCS)) }
+            .setNegativeButton(R.string.cancel) { _, _ -> finish() }
+            .show()
+    }
+
+    private fun openDocumentDetails(document: Document) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(document.url))
+        startActivity(intent)
+    }
+
+    private fun onPopUpMenuClick(documentId: Int, action: Int, position: Int) {
+        when (action) {
+            RENAME_ACTION -> showRenameDialog(documentId, position)
+            DELETE_ACTION -> deleteDocument(documentId, position)
+        }
+    }
+
+    private fun showRenameDialog(documentId: Int, position: Int) {
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        AlertDialog.Builder(this)
+            .setTitle(R.string.insert_title)
+            .setView(input)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                renameDocument(documentId, input.text.toString(), position)
+            }
+            .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.cancel() }
+            .show()
+        (input.layoutParams as FrameLayout.LayoutParams).marginStart = 50
+        (input.layoutParams as FrameLayout.LayoutParams).marginEnd = 50
+    }
+
+    private fun renameDocument(documentId: Int, documentTitle: String, position: Int) {
+        containerView.isRefreshing = true
+        VK.execute(DocumentsEdit(documentId, documentTitle), object : VKApiCallback<Int> {
+            override fun success(result: Int) {
+                if (!isFinishing && result == 1) {
+                    containerView.isRefreshing = false
+                    documentsAdapter.renameDocument(documentTitle, position)
+                }
+            }
+
+            override fun fail(error: Exception) {
+                showError()
+            }
+        })
+    }
+
+    private fun deleteDocument(documentId: Int, position: Int) {
+        containerView.isRefreshing = true
+        val userId = VKAccessToken.restore(getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE))?.userId
+        if (userId == null) {
+            showError()
+            return
+        }
+        VK.execute(DocumentsDelete(userId, documentId), object : VKApiCallback<Int> {
+            override fun success(result: Int) {
+                if (!isFinishing && result == 1) {
+                    containerView.isRefreshing = false
+                    documentsAdapter.deleteDocument(position)
+                }
+            }
+
+            override fun fail(error: Exception) {
+                showError()
+            }
+        })
+    }
+
+    companion object {
+        const val RENAME_ACTION = 0
+        const val DELETE_ACTION = 1
+
+        private const val PREFERENCE_NAME = "com.vkontakte.android_pref_name"
+
+        fun createIntent(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            context.startActivity(intent)
+        }
+    }
+}
